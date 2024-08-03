f942790992eb0ca93801ed155258e120da4a43d4
cramps-man <cramps-man@hotmail.com>
2024-08-03 22:54:29 +1000

    Add autosave toggle
    - Still need to trigger an autosave when anything changes

--- Main.gd
+++ Main.gd
@@ -10,12 +10,18 @@ func _ready() -> void:
 		child.load(current_data)
 	for child in $GridContainer2.get_children():
 		child.load(current_data)
+	
+	if current_data.has("should_autosave"):
+		Autosave.autosave = current_data.should_autosave
+		update_autosave_button_text()
 
 func _on_save_button_pressed() -> void:
 	for child in $GridContainer.get_children():
 		child.save(current_data)
 	for child in $GridContainer2.get_children():
 		child.save(current_data)
+	
+	current_data.should_autosave = Autosave.autosave
 
 	var save_game = FileAccess.open("user://savegame.save", FileAccess.WRITE)
 	save_game.store_line(JSON.stringify(current_data))
@@ -29,3 +35,13 @@ func _on_popup_menu_id_pressed(id: int) -> void:
 			child.reset()
 		for child in $GridContainer2.get_children():
 			child.reset()
+
+func _on_autosave_button_pressed() -> void:
+	Autosave.autosave = !Autosave.autosave
+	update_autosave_button_text()
+
+func update_autosave_button_text() -> void:
+	if Autosave.autosave:
+		$AutosaveButton.text = "Autosave ON"
+	else:
+		$AutosaveButton.text = "Autosave OFF"
---
+++ autoloads/autosave.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var autosave := false

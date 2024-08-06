13627b6267c36b2fa97a8e8adaadedf707df2faa
cramps-man <cramps-man@hotmail.com>
2024-08-07 04:04:50 +1000

    Add version number display + lessen save message distance

--- save_message.gd
+++ save_message.gd
@@ -3,7 +3,7 @@ extends Label
 func animate_save_message(start_position: Vector2) -> void:
 	position = start_position
 	var tween := create_tween()
-	tween.tween_property(self, "position", position + Vector2(0, -50), 1.0)
+	tween.tween_property(self, "position", position + Vector2(0, -25), 1.0)
 	tween.finished.connect(func(): 
 		queue_free()
 	)
